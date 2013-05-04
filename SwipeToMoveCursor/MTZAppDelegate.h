44e0d0c237a5b2b093e92389125e39203cbdecb9
Matt Zanchelli <matt@mdznr.com>
2013-05-04 12:11:36 -0400

    Initial Commit

---
+++ SwipeToMoveCursor/MTZAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MTZAppDelegate.h
+//  SwipeToMoveCursor
+//
+//  Created by Matt on 5/4/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MTZViewController;
+
+@interface MTZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MTZViewController *viewController;
+
+@end
---
+++ SwipeToMoveCursor/MTZViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTZViewController.h
+//  SwipeToMoveCursor
+//
+//  Created by Matt on 5/4/13.
+//  Copyright (c) 2013 Matt Zanchelli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTZViewController : UIViewController
+
+@end
